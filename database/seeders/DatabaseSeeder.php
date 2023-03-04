4baede98f52726c9486ee84283d066c876f8e4bd
fielmangginimba <fielmangginimba@gmail.com>
2023-03-04 20:19:37 +0800

    first and complete commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +14,25 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(5)->create();
+
+        DB::table('contacts')->insert([
+            'name'          =>  'Taylor Swift',
+            'address'       =>  'Malaybalay City, Bukidnon',
+            'mobile'          =>  '09978584850'
+        ]);
+
+        DB::table('contacts')->insert([
+            'name'          =>  'Justine Bieber',
+            'address'       =>  'Cabanglasan, Bukidnon',
+            'mobile'          =>  '09075354624'
+        ]);
+
+        DB::table('contacts')->insert([
+            'name'          =>  'Maria Clara Garcia',
+            'address'       =>  'Valencia City, Bukidnon',
+            'mobile'          =>  '09347675878'
+        ]);
+
     }
 }
