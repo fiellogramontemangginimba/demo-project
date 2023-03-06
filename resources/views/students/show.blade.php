4e1a0dfd8543459cca0ff3371bba18f332c6bc72
fielmangginimba <fielmangginimba@gmail.com>
2023-03-06 20:43:29 +0800

    crud application

---
+++ resources/views/students/show.blade.php
@@ -0,0 +1,16 @@
+@extends('students.layout')
+@section('content')
+<div class="card">
+  <div class="card-header">Contactus Page</div>
+  <div class="card-body">
+  
+        <div class="card-body">
+        <h5 class="card-title">Name : {{ $students->name }}</h5>
+        <p class="card-text">Address : {{ $students->address }}</p>
+        <p class="card-text">Phone : {{ $students->mobile }}</p>
+  </div>
+      
+    </hr>
+  
+  </div>
+</div>
